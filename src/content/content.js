1eb2804f39ae26657bae782b11375c8d4a1b4be4
Cuyler Stuwe <salembeats@gmail.com>
2020-08-27 14:44:40 -0700

    aha 2

--- src/content/content.js
+++ src/content/content.js
@@ -5,6 +5,8 @@ const CONTENT_SCRIPT_INSTANCE_ID = Math.floor(Math.random() * 1000);
 async function main() {
     window.addEventListener("message", () => {
         console.log("Number is", CONTENT_SCRIPT_INSTANCE_ID);
+
+        chrome.runtime.sendMessage({type: "START_OVER"}, () => {});
     });
 }
 
--- src/modules/backgroundListenerModules.js
+++ src/modules/backgroundListenerModules.js
@@ -1,3 +1,7 @@
 export default [
-
+    {
+        ["START_OVER"]: async (argsObj) => {
+            chrome.runtime.reload();
+        }
+    }
 ];
