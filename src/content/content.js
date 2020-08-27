78b0b6f7f77e6d29c0cfd89887fc362fb3cec571
Cuyler Stuwe <salembeats@gmail.com>
2020-08-27 14:31:43 -0700

    aha

--- src/content/content.js
+++ src/content/content.js
@@ -1,6 +1,11 @@
 import "../modules/startedLog";
 
+const number = Math.floor(Math.random() * 1000);
+
 async function main() {
+    window.addEventListener("message", () => {
+        console.log("Number is", number);
+    });
 }
 
 main();
